c3bf00b8e2ff74e1f6e299358a17261951a7e4e0
Rocky Li <liqiuyi1991@gmail.com>
2016-04-16 19:26:24 -0500

    04/16/2016-update

--- WordPattern.java
+++ WordPattern.java
@@ -9,28 +9,31 @@
   pattern = "aaaa", str = "dog cat cat dog" should return false.
   pattern = "abba", str = "dog dog dog dog" should return false.
  */
-//29 test cases all passed, 9ms solution
+//29 test cases all passed, 3ms solution
 public class Solution {
     public static boolean wordPattern (String pattern, String str) {
         if (pattern.length() == 0 || str.length() == 0) return false;
 
+        String[] words = str.split(" ");
+
+        if (pattern.length() != words.length) return false;
+
         Map<String, Character> hashMap = new HashMap<String, Character>();
-        String comparePat = "";
+        StringBuilder comparePat = new StringBuilder();
         int tracker = 0;
 
-        for (String s : str.split("\\W+")) {
-            if (hashMap.containsKey(s)) {
-                comparePat += hashMap.get(s) + "";
+        while (tracker < pattern.length()) {
+            if (hashMap.containsKey(words[tracker])) {
+                comparePat.append(hashMap.get(words[tracker])+"");
             } else {
-                if (hashMap.containsValue(pattern.charAt(tracker)) || tracker >= pattern.length()) return false;
-                hashMap.put(s, pattern.charAt(tracker));
-                comparePat += pattern.charAt(tracker)+"";
+                if (hashMap.containsValue(pattern.charAt(tracker))) return false;
+                hashMap.put(words[tracker], pattern.charAt(tracker));
+                comparePat.append(pattern.charAt(tracker)+"");
             }
             tracker++;
         }
-        System.out.println(comparePat);
 
-        if (comparePat.equals(pattern)) return true;
+        if (comparePat.toString().equals(pattern)) return true;
         return false;
     }
 }
